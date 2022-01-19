81091f409bbc1e92ed32ae414d39797c36701d39
Shobhit Patel <shobhit.patel101@gmail.com>
2022-01-19 11:03:39 +0530

    Updated header footer

---
+++ resumebuilder/src/footer.js
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h1 style={{textAlignVertical: "center",textAlign: "center",}}>@2022</h1>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ resumebuilder/src/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import $ from 'jquery';
+
+function Header(){
+    return (
+    <div className="header">
+      <h1 style={{textAlignVertical: "center",textAlign: "center",}}>Resume Builder</h1>
+    </div>
+  );
+}
+
+
+export default Header;
