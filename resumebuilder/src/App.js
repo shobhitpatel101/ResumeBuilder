99d784aa03232aaafe9368ed60a849f3a351eb84
Shobhit Patel <shobhit.patel101@gmail.com>
2022-01-20 22:29:52 +0530

    added bootstrap

--- resumebuilder/src/App.js
+++ resumebuilder/src/App.js
@@ -2,18 +2,35 @@ import React from 'react'
 import './App.css';
 import Header from './header';
 import Footer from './footer';
+import {Container,Col,Row} from 'react-bootstrap'
+
+//componentDidMount() {}
 
 function App() {
   return (
     <div>
     <Header />
     
-    <div className="field">
-      <label for="fname">First name:</label><br/>
-      <input type="text" id="fname" name="fname" value="John"/>
-      <br/>
-      <button id="submit">Submit</button>
-    </div>
+    <Container className='mainTable'>
+      <Row>
+        <Col><p>First Name</p></Col>
+        <Col><p>Middle Name</p></Col>
+        <Col><p>Last Name</p></Col> 
+      </Row>
+      <Row>
+        <Col><input type='Text' placeholder='First Name'></input></Col>
+        <Col><input type='Text' placeholder='Middle Name'></input></Col>
+        <Col><input type='Text' placeholder='Last Name'></input></Col>
+      </Row>
+
+      <Row><Col>.</Col></Row> 
+      
+      <Row>
+        <Col><p>Bio Details</p></Col>
+        <Col><textarea></textarea></Col>
+      </Row>
+
+  </Container>
 
     <Footer />
     </div>
